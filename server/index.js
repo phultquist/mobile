2fc2907b92e6955b83942276d9a398f3b2404656
phultquist <phultquist@imsa.edu>
2020-09-01 16:38:37 -0500

    addison ip

--- server/index.js
+++ server/index.js
@@ -3,26 +3,31 @@ const fs = require('fs')
 
 const wss = new WebSocket.Server({ port: 8080 })
 
-const defaults = {
-    contrast: 10,
-    brightness: 20,
-    animation: 12,
-    fromServer: true
-}
+fs.readFile('settings.json', 'utf8', (err, data) => {
+  console.log(data);
+  const defaults = JSON.parse(data);
 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log(message)
-    let values = JSON.parse(message);
-    let filetext = `
-# note this only stores the raw values 0 - 100. they are interpreted in the python script
-brightness = "${values.brightness}"
-contrast = "${values.contrast}"
-animation = "${values.animation}"
-    `
-    fs.writeFile('settings.json', message, function() {
-      return true
+  wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+      console.log(message)
+      let values = JSON.parse(message);
+      let filetext = `
+  # note this only stores the raw values 0 - 100. they are interpreted in the python script
+  brightness = "${values.brightness}"
+  contrast = "${values.contrast}"
+  animation = "${values.animation}"
+      `
+      fs.writeFile('settings.json', message, function () {
+        return true
+      })
     })
+    ws.send(JSON.stringify(defaults))
   })
-  ws.send(JSON.stringify(defaults))
 })
+// const defaults = {
+//     contrast: 10,
+//     brightness: 20,
+//     animation: 12,
+//     fromServer: true
+// }
+
