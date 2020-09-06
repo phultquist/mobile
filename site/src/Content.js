eb74a01aaee045b3e18e363affac620e82f07af6
phultquist <phultquist@imsa.edu>
2020-09-06 00:26:00 -0500

    unnceseccary components

--- site/src/Content.js
+++ site/src/Content.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Slider, Button, Select } from 'theme-ui'
-import theme from './theme'
+import { Slider, Select } from 'theme-ui'
+// import theme from './theme'
 
 export class Content extends React.Component {
 
@@ -12,12 +12,14 @@ export class Content extends React.Component {
             contrast: 0,
             brightness: 0,
             animation: 0,
-            sound: 'true',
+            useAnimation: true,
+            autobrightness: true,
             fromServer: false
         }
         this.onChange = this.onChange.bind(this)
         this.onFieldChange = this.onFieldChange.bind(this)
         this.onUndo = this.onUndo.bind(this)
+        this.ready = false;
     }
 
     componentDidMount() {
@@ -30,9 +32,9 @@ export class Content extends React.Component {
     componentDidUpdate() {
         // console.log(this.props.key);
         // this.setState(this.props.key)
-        console.log(this.props);
         if (this.props.defaults.fromServer === true) {
             this.props.defaults.fromServer = false
+            this.ready = true;
             this.setState(this.props.defaults)
             console.log(this.state);
         }
@@ -54,7 +56,7 @@ export class Content extends React.Component {
         let index = event.target.selectedIndex
         let value = event.target.children[index].value
         console.log(value);
-        this.setState({[event.target.id]: value})
+        this.setState({ [event.target.id]: value })
         this.onChange();
     }
 
@@ -65,6 +67,25 @@ export class Content extends React.Component {
 
     render() {
         // console.log('rendering content', this.props.defaults);
+        if (!this.ready) {
+            console.log('not ready');
+            return (
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        // alignItems: 'center',
+                        height: 'auto',
+                    }}>
+                    <div style={{
+                        width: '90vw'
+                    }}>
+                        <h2>Connecting...</h2>
+                    </div>
+                </div >
+            )
+        }
+
         return (<div
             style={{
                 display: 'flex',
@@ -75,23 +96,29 @@ export class Content extends React.Component {
             <div style={{
                 width: '90vw'
             }}>
-                <h2>Paused/Playing/Off/Error</h2>
+                <h2>Settings</h2>
+
+                <h4 style={fieldTitle}>Brightness</h4>
+                <Slider id='brightness'  value={this.state.brightness} onChange={this.onFieldChange} />
 
                 <h4 style={fieldTitle}>Contrast</h4>
                 <Slider id='contrast' value={this.state.contrast} onChange={this.onFieldChange} />
 
-                <h4 style={fieldTitle}>Brightness</h4>
-                <Slider id='brightness' value={this.state.brightness} onChange={this.onFieldChange} />
-
-                <h4 style={fieldTitle}>Animation Length</h4>
-                <Slider id='animation' value={this.state.animation} onChange={this.onFieldChange} />
+                <h4 style={fieldTitle}>Fade</h4>
+                <Slider id='animation' min={5} value={this.state.animation} onChange={this.onFieldChange} />
 
                 <h4 style={fieldTitle}>Animation</h4>
-                <Select name='useAnimation' id='useAnimation' mb={3} onChange={this.onSelectChange}>
+                <Select name='useAnimation' id='useAnimation' mb={3} value={this.state.useAnimation} onChange={this.onSelectChange}>
                     <option value='true'>On</option>
                     <option value='false'>Off</option>
                 </Select>
-                <Button theme={theme} variant='secondary' onClick={this.onUndo}>Undo</Button>
+
+                <h4 style={fieldTitle}>Auto Brightness</h4>
+                <Select name='autobrightness' id='autobrightness' mb={3} value={this.state.autobrightness} onChange={this.onSelectChange}>
+                    <option value='true'>On</option>
+                    <option value='false'>Off</option>
+                </Select>
+                {/* <Button theme={theme} variant='secondary' onClick={this.onUndo}>Undo</Button> */}
             </div>
         </div>);
     }
